59713483761b3364835e24ec911b5f152cc28806
Hazieon <handrew3721@gmail.com>
2021-04-17 12:51:41 +0100

    WIP - image and text for about us section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,13 +3,16 @@ import styles from "./index.module.css"
 function Homepage() {
   return (
     <div className="Homepage">
-   <div class={styles.heroImage}>
-    <div class={styles.heroText}>
-    <h1>Extraordinary people</h1>
-    <p>doing extra ordinary things.</p>
-    <button>Start Renting</button>
-  </div>
-</div> 
+        <div class={styles.heroImage}>
+            <div class={styles.heroText}>
+             <h1>Extraordinary people</h1>
+            <h2>doing extraordinary things.</h2>
+             <button class={styles.heroButton}>Explore nearby</button>
+             </div>
+        </div> 
+    
+
+    <img className={styles.aboutImage} src={"/images/earth.png"} alt="ecycle community member"/>
     </div>
   );
 }
